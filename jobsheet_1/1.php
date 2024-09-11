b6b49ea2bfdc440f0e1399fdf5e247a5d2523925
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-09-11 21:14:05 +0700

    1.php

--- jobsheet_1/1.php
+++ jobsheet_1/1.php
@@ -6,21 +6,21 @@ class Mahasiswa {
     public $nim;
     public $jurusan;
 
-    // Constructor (Menginisialisasi class yang baru)
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan;
-    }
-
     //Metode atau function
      public function tampilkanData() {
         return "Nama : $this->nama, NIM : $this->nim, Jurusan : $this->jurusan";
      }
 }
 
-//instansiasi objek
-$mahasiswa1 = new Mahasiswa("Ana Febri Salusi", "230202027", "Jurusan Komputer dan Bisnis");
+// Instansiasi objek dari class Mahasiswa
+$mahasiswa1 = new Mahasiswa();
+
+// Menetapkan nilai untuk atribut nama, nim, dan jurusan
+$mahasiswa1->nama = "Ana Febri Salusi";
+$mahasiswa1->nim = "230202027";
+$mahasiswa1->jurusan = "Teknik Informatika";
+
+// Memanggil metode tampilkanData untuk menampilkan data mahasiswa
 echo $mahasiswa1->tampilkanData();
 
 
