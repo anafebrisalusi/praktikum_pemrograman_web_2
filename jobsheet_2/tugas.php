c1224dcee88524d06423c32901370f0294ef76f7
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-09-11 21:26:33 +0700

    tugas.php

--- jobsheet_2/tugas.php
+++ jobsheet_2/tugas.php
@@ -1,31 +1,25 @@
 <?php
 // 1. Implementasi kelas Dosen dengan atribut nama, nip, dan mataKuliah
-// class dosen
+// class Dosen
 class Dosen {
     public $nama;
     public $nip;
     public $mataKuliah;
 
-    //constructor
-    public function __construct($nama, $nip, $mataKuliah) {
-        $this->nama= $nama;
-        $this->nip= $nip;
-        $this->mataKuliah= $mataKuliah;
-    }
-
-/* 2. Buat Metode tampilkanDosen() untuk 
-menampilkan informasi dosen
-*/
+    // Metode untuk menampilkan informasi dosen
     public function tampilkanDosen() {
         return "Nama Dosen : {$this->nama} <br> NIP : {$this->nip} <br> Mata Kuliah : {$this->mataKuliah}<br>";
     }
 }
 
-/* 3. Buat objek dari kelas Dosen, 
-dan gunakan metode tampilkanDosen() untuk
-menampilkan informasi tersebut.
+/* 2. Buat objek dari kelas Dosen, 
+dan set atribut secara manual
 */
+$dosen1 = new Dosen();
+$dosen1->nama = "Aprilia";
+$dosen1->nip = "230202047";
+$dosen1->mataKuliah = "Filsafat";
 
-$dosen1= new Dosen("Aprilia", "230202047", "Filsafat");
+// Tampilkan informasi dosen
 echo $dosen1->tampilkanDosen();
 ?>
