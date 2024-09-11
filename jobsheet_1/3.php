bbe013ba9cda4a5954b1e4270fd04e01f4f85ba2
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-09-11 21:15:41 +0700

    3.php

--- jobsheet_1/3.php
+++ jobsheet_1/3.php
@@ -1,35 +1,27 @@
 <?php
-// 3. Enheritance
-// Membuat class Pengguna
+// Class Pengguna
 class Pengguna {
     public $nama;
 
-    // Constructor (Menginisialisasi class yang baru)
-    public function __construct($nama) {
-        $this->nama= $nama;
-    }
-
-    // metode getNama()
-    public function getNama(){
+    public function getNama() {
         return $this->nama;
     }
 }
-    // membuat class dosen yang mewarisi class pengguna
-    class Dosen extends Pengguna {
-        private $mataKuliah;
 
-    public function __construct($nama, $mataKuliah) {
-        parent::__construct($nama);
-        $this->mataKuliah = $mataKuliah;
-    }
+// Class Dosen mewarisi Pengguna
+class Dosen extends Pengguna {
+    public $mataKuliah;
 
-     // Menampilkan data dosen
-     public function tampilkanData() {
-        return "Nama Dosen: {$this->nama}<br> Mata Kuliah: {$this->mataKuliah}<br>";
+    public function getDataDosen() {
+        return "Nama Dosen: " . $this->getNama() . ", Mata Kuliah: " . $this->mataKuliah;
     }
+}
 
-    }
-//instansiasi class Dosen
-$dosen = new Dosen("Ana Febri Salusi", "Bahasa Jawa");
-echo $dosen->tampilkanData();
+// Instansiasi objek Dosen dan set atribut
+$dosen1 = new Dosen();
+$dosen1->nama = "Dr. Budi";
+$dosen1->mataKuliah = "Pemrograman PHP";
+
+// Tampilkan data dosen
+echo $dosen1->getDataDosen();
 ?>
